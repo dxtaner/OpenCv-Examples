3f723647220efa19a511eef3eef1e555504e5cb7
Taner <tanerozer16@gmail.com>
2022-02-07 14:44:33 +0300

    Resim Boyutlandirma

---
+++ ResimOkumaİşlemleri/resim_boyutlandirma.py
@@ -0,0 +1,23 @@
+import cv2
+
+def main():
+    img= cv2.imread('messi.jpg')
+    ekran_cozunulurluk=680,460
+
+    skala_genislik=ekran_cozunulurluk[0]/img.shape[1]
+    skala_yukseklik=ekran_cozunulurluk[1]/img.shape[0]
+    skala=min(skala_genislik,skala_yukseklik)
+
+    pencere_genislik=int(img.shape[1]*skala)
+    pencere_yukseklik = int(img.shape[0]*skala)
+
+    cv2.namedWindow('Boyutlanabilir Pencere',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Boyutlanabilir Pencere',pencere_genislik,pencere_yukseklik)
+
+    cv2.imshow('Boyutlanabilir Pencere',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__=="__main__":
+    main()
