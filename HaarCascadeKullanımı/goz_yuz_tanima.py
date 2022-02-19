e22fd37b6993676f9c5cc654e0263e90ebf5616b
Taner Özer <tanerozer16@gmail.com>
2022-02-19 16:11:34 +0300

    faceandeyes

---
+++ HaarCascadeKullanımı/goz_yuz_tanima.py
@@ -0,0 +1,29 @@
+import cv2
+
+yuz_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+goz_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+
+kamera=cv2.VideoCapture(0)
+
+while True:
+    cm,goruntu=kamera.read()
+
+    griton=cv2.cvtColor(goruntu,cv2.COLOR_BGR2GRAY) #gritonlama
+    yuzler=yuz_cascade.detectMultiScale(griton,1.3,5)
+
+    for (x,y,w,h) in yuzler:
+        cv2.rectangle(goruntu,(x,y),(x+w,y+h),(0,0,255),3)
+        griton=griton[y:y+h,x:x+w]
+        renkli=goruntu[y:y+h,x:x+w]
+        gozler=goz_cascade.detectMultiScale(griton)
+
+        for(ex,ey,ew,eh) in gozler:
+            cv2.rectangle(renkli,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+
+    cv2.imshow('goruntu',goruntu)
+
+    if cv2.waitKey(13) &0xFF==ord('q'):
+        break
+    
+kamera.release()
+cv2.destroyAllWindows()
