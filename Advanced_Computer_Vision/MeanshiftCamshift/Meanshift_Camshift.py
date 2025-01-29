70d7726db53f5ceb838ae46fc0f3edadfd2bdd76
Taner Özer 😌 <tanerozer16@gmail.com>
2025-01-29 18:17:22 +0300

    Create Meanshift_Camshift.py

---
+++ Advanced_Computer_Vision/MeanshiftCamshift/Meanshift_Camshift.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)  # Access the default camera (index 0)
+
+ret, frame = cap.read()
+if ret == False:
+    print("Warning: Unable to access the camera!")
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_rect = face_cascade.detectMultiScale(frame)
+
+(face_x, face_y, w, h) = tuple(face_rect[0])
+track_window = (face_x, face_y, w, h)  # Initial tracking window for MeanShift
+
+roi = frame[face_y:face_y + h, face_x:face_x + w]  # Crop the face region
+hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)  # Convert ROI to HSV color space
+
+roi_hist = cv2.calcHist([hsv_roi], [0], None, [180], [0, 180])
+cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)  # Normalize histogram
+
+term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5, 1)
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+
+        ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+
+        x, y, w, h = track_window
+        img2 = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 5)
+
+        cv2.imshow("Tracking", img2)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
